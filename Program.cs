cd53e9433a8de234bdbf1806e9d6aafaa19b92d6
jerome.clauet <clauet.jerome@hotmail.fr>
2021-01-16 13:13:25 +0100

    Exercice 3

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Exercice_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 0;
+            int result;
+            string response = "oui";
+            while (response != "non")
+            {
+                Console.WriteLine("La valeur de nombre est " + number);
+                Console.WriteLine("Ajoutez un nombre que vous voulez additionner :");
+                int sum = int.Parse(Console.ReadLine());
+                result = number + sum;
+                Console.WriteLine(result);
+                number = result;
+                Console.WriteLine("Souhaitez vous ajouter un autre nombre à votre opération ?");
+                response = Console.ReadLine();
+            }
+        }
+    }
+}
